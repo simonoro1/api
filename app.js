46801c494fe3ff767a1d60a48dcdab8fc25d9aa7
Simon Oro <simonoro1@outlook.com>
2023-10-03 10:25:09 -0300

    api set up

---
+++ app.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const mongoString = process.env.DATABASE_URL;
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on("error", (error) => {
+  console.log(error);
+});
+
+database.once("connected", () => {
+  console.log("Database connected");
+});
+
+const app = express();
+const port = 3000;
+
+// app use
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
